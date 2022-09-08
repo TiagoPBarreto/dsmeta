d67f03fd01fd40e89b0abfad51cf5fbca54578d0
Abfna123 <98656597+Abfna123@users.noreply.github.com>
2022-09-08 17:23:02 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import NotificationButton from "./componentes/notificationButton"
+import Header from "./componentes/Header"
+import SalesCard from "./componentes/SalesCard"
+
 
 
 function App() {
   return (
     <>
-      <h1>Ola mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
